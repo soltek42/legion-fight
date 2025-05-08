d01d080eba1a42b64e24e84970a7c432d4c147c7
Denys-Ivko_shell <denys.ivko@shell.com>
2025-05-08 19:37:42 +0200

    fix?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path, { dirname } from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 import glsl from "vite-plugin-glsl";
 
@@ -11,7 +10,6 @@ const __dirname = dirname(__filename);
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
     glsl(), // Add GLSL shader support
   ],
   resolve: {
